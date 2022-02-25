b87ce6c7cd17b41e1c23fca7053d288bb8637b6c
Aditya Kishore <30798316+BATspock@users.noreply.github.com>
2022-02-25 13:01:06 +0530

    prepare data from txt input file

--- BFS.py
+++ BFS.py
@@ -1,4 +1,22 @@
 import queue 
 import sys
 
+def prepareForBFS(l, goal):
+        nodeInfo = {}
+        #visited = {}
+        parent = {}
+
+        for _ in range(5, int(l[4]) + 5):
+            t = list(map(int,(l[_].split())))
+            nodeInfo[tuple(t[:3])] = t[3:]
+            #visited[tuple(t[:3])]= 0
+            parent[tuple(t[:3])] = tuple(t[:3])
+            
+        if goal not in nodeInfo:
+            nodeInfo[goal] = [-1]
+            #visited[goal] = 0
+            parent[goal] = goal
+
+
+        return nodeInfo, parent
 
