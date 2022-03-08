dee243ed6462728202617c4fcdfc85531794667b
BATspock <aditya20kishore@gmail.com>
2022-03-07 17:32:46 -0800

    added BFS code

--- BFS.py
+++ BFS.py
@@ -20,3 +20,39 @@ def prepareForBFS(l, goal):
 
         return nodeInfo, parent
 
+
+def BFS(nodeInfo, parent, beg, goal):
+        
+        if (beg not in nodeInfo) : return "FAIL"
+        
+        if beg == goal: return -1
+        
+        visited= set()
+        q.put(beg)
+        #visited[beg] = 1
+        visited.add(beg)
+
+        while(not q.empty()):
+    
+            node = q.get()
+            #print(node)
+
+            if (node == goal):
+                return parent 
+
+            for _ in nodeInfo[node]:
+                #print(node)
+                #newNode = tuple(np.array(list(node)) + np.array(act[_-1]))
+                
+                #n = list(node)
+                newNode = ((node[0]+act[_-1][0], node[1]+act[_-1][1], node[2]+act[_-1][2]))
+                if newNode in nodeInfo:
+                    if  newNode in visited:
+                        continue 
+                    else:
+                        visited.add(newNode)
+                        parent[newNode] = node
+                        q.put(newNode)
+
+
+        return ("FAIL")
