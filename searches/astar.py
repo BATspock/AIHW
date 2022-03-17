f673d6b3042acabbb4a9d4077ce396f582cb6086
BATspock <aditya20kishore@gmail.com>
2022-03-17 13:02:53 -0700

    astar algorithm

--- searches/astar.py
+++ searches/astar.py
@@ -28,3 +28,68 @@ def prepareForAstar(l, goal):
         #visited[goal] = 0    
     del heuristicCost    
     return nodeInfo, nodeInfoCost#, visited, #heuristicCost#, parent
+
+
+def Astar(nodeInfo, nodeInfoCost, beg, goal):
+    
+    if (beg not in nodeInfo) : return "FAIL"
+    if beg == goal: return -1
+    
+    l = []
+    heapq.heappush(l, nodeInfoCost[beg])        
+    costToGoal = nodeInfoCost[goal][1]
+    visited = set()
+    
+    while(not len(l) == 0 and _check(l, costToGoal)): 
+        
+        node = heapq.heappop(l)
+
+        if node[2] == goal:
+            
+            if node[1] < costToGoal:
+                costToGoal = node[1]
+                
+        else:
+            for _ in nodeInfo[node[2]]:
+
+                #newNode = tuple(np.array(node[2]) + np.array(act[_-1]))
+                #n = list(node)
+                newNode = ((node[2][0]+act[_-1][0], node[2][1]+act[_-1][1], node[2][2]+act[_-1][2]))
+                
+                if newNode in nodeInfo:
+                    if newNode not in visited:
+
+                        if (_-1)>5:
+                            #update actual cost
+                            hcost = nodeInfoCost[node[2]][1] + nodeInfoCost[newNode][0]+14
+                            cost = nodeInfoCost[node[2]][1] +14
+                            newNode = ((hcost, cost, newNode, tuple(node[2]), 14))
+
+                            if (hcost < costToGoal):                                
+                                heapq.heappush(l, newNode)
+                                
+                            if (nodeInfoCost[newNode[2]][1]>cost):
+                                nodeInfoCost[newNode[2]] = newNode
+                                
+                        else:
+                            
+                            hcost = nodeInfoCost[node[2]][1] + nodeInfoCost[newNode][0] +10
+                            cost = nodeInfoCost[node[2]][1]+10
+                            newNode = ((hcost, cost, newNode, tuple(node[2]), 10))
+
+                            if (hcost < costToGoal):
+                                heapq.heappush(l, newNode)
+
+                            if (nodeInfoCost[newNode[2]][1]>cost):
+                                nodeInfoCost[newNode[2]] = newNode                                
+                                
+
+
+        if (node[2] != goal):
+                #visited[node[2]] = 1
+                visited.add(node[2])
+            
+    if (nodeInfoCost[goal][1] == sys.maxsize):
+        return "FAIL"
+    
+    return nodeInfoCost
